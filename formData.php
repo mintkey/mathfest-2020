f7767a9176bd08835afedfeab0a27161bb44990a
Helen Dempsey <40031593+mintkey@users.noreply.github.com>
2020-01-31 04:25:22 -0500

    Refactor code and add schedule
    Updates to media queries and added schedule information

--- formData.php
+++ formData.php
@@ -38,60 +38,16 @@ $mail = new PHPMailer;
 $mail->SMTPDebug = 2; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
 $mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
 $mail->Port = 465; // TLS only
-$mail->SMTPSecure = 'tls'; // ssl is depracated
+$mail->SMTPSecure = 'TLS'; // SSL is depracated
 $mail->SMTPAuth = true;
 $mail->Username = 'cbreiner@fordham.edu';
-$mail->Password = 'TYPE PASSWORD HERE';
+$mail->Password = 'TYPE PASSWORD HERE'; // Password needed
 $mail->setFrom($emailFrom, $emailFromName);
 $mail->addAddress($emailTo, $emailToName);
-$mail->Subject = "You've successfully registered for MathFest 2020!";
-$mail->Body = "Testing 123"; // CHANGE //
+$mail->Subject = "You're registered for MathFest 2020!";
+// Body needed
 
 $mail->send();
-
-// ??? //
-/*
-if(!$mail->send()){
-    echo "Mailer Error: " . $mail->ErrorInfo;
-}else{
-    echo "Message sent!";
-} */
-
-/*
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
-    $headers .= "From: cwestby1@gmail.com" . "\r\n" .
-    "Reply-To: cwestby1@gmail.com" . "\r\n" .
-    "X-MSMail-Priority: High" . "\r\n" .
-    "X-Mailer: PHP/" . phpversion();
-    $drBriener = "cwestby1@fordham.edu";
-    $emailAddress = 'cwestby1@fordham.edu';
-	$subject = "$firstName $lastName signed up for MathFest 2019!";
-    $regInfo = json_encode($formdata);
-    $thankYou = "Thank you, $firstName for signing up for MathFest 2019!";
-    $confMessage = '<html><body>';
-    $confMessage .= '<header class="masthead" style="background-image: url(\'img/dreamstime_xxl_64717876.jpg\')">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 col-md-10 mx-auto">
-            <div class="site-heading">
-              <h2>You\'re registered for MathFest 2019!</h2>
-              <h4><b>Saturday, April 6th</b></h4>
-              <p id="timer"></p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>';
-    $confMessage .= '<h3>We look forward to seeing you on April 6th! Further information on the event and how to get there can be found at faculty.fordham.edu/cbreiner/mathfest2019</h3>';
-    $confMessage .= '</body></html>';
-    if (mail($drBriener, $subject, $regInfo, $headers) && mail($emailAddress, $thankYou, $confMessage, $headers)){
-		$msg = "Thanks for filling out our form";
-    }
-	else{
-		$msg = "Problem sending the message";
-	} */
 }
 
 ?>
--- js/googlescript-ajax.js
+++ js/googlescript-ajax.js
@@ -1,3 +1,4 @@
+// Exports HTML form data to Google Sheet
 var $form = $('#register-form'),
   url = 'https://script.google.com/macros/s/AKfycbw2I4X3JdjDteLrLZfC74BbWCJzgCBK5ayTt4kCStw5leK868Fq/exec'
 
